35cd61e8d9287d99fdedc0fe820c15275c41637f
piresp <gabrielfpp47@gmail.com>
2023-01-02 14:53:24 -0300

    one_page_rendering

--- react_v/exercice_1/src/App.jsx
+++ react_v/exercice_1/src/App.jsx
@@ -1,22 +1,64 @@
 import { useState } from "react";
-import FirstPage from "./pages/FirstPage";
-import SecondPage from "./pages/SecondPage";
-import ThirdPage from "./pages/ThirdPage";
+
+import { ReactComponent as FirstImage } from "./assets/1_window.svg";
+import { ReactComponent as SecondImage } from "./assets/2_window.svg";
+import { ReactComponent as ThirdImage } from "./assets/3_window.svg";
+
+import Body from "./components/Body";
+import MainText from "./components/MainText";
+import Point from "./components/Point";
+import ContinueButton from "./components/ContinueButton";
+import SkipButton from "./components/SkipButton";
 
 export default function App() {
+  const pages = [
+    {
+      firstPage: [
+        <FirstImage />,
+        <MainText
+          title={`Plant lover Community`}
+          text={`Find gardening enthusiasts from all over the world`}
+        />,
+      ],
+      secondPage: [
+        <SecondImage />,
+        <MainText
+          title={`Get fast & safe delivery`}
+          text={`Get good quality products for your plants`}
+        />,
+      ],
+      thirdPage: [
+        <ThirdImage />,
+        <MainText
+          title={`Buy & Sell Tools`}
+          text={`Buy & sell good quality products for your beautiful plants`}
+        />,
+      ],
+    },
+  ];
+
+  const firstPage = pages[0].firstPage;
+  const secondPage = pages[0].secondPage;
+  const thirdPage = pages[0].thirdPage;
 
   const [value, setValue] = useState(0);
 
-  const handleValueChange = newValue => {
+  const handleValueChange = (newValue) => {
     setValue(newValue);
   };
-  
+
   return (
-    <div>
-      {value === 0 ? <FirstPage onValueChange={handleValueChange} page={value} /> : null}
-      {value === 1 ? <SecondPage onValueChange={handleValueChange} page={value} /> : null}
-      {value === 2 ? <ThirdPage onValueChange={handleValueChange} page={value} /> : null}
-      {value === 3 ? <FirstPage onValueChange={handleValueChange} page={value} /> : null }
-    </div>
+    <Body>
+      {value === 0
+        ? firstPage.map((component) => component)
+        : value === 1
+        ? secondPage.map((component) => component)
+        : value === 2
+        ? thirdPage.map((component) => component)
+        : null}
+      <Point page={value} />
+      <ContinueButton onValueChange={handleValueChange} page={value} />
+      <SkipButton />
+    </Body>
   );
 }
