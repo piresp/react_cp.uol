2a47b25a154ff4c71a77575409c28298ec335735
piresp <gabrielfpp47@gmail.com>
2023-01-02 14:54:13 -0300

    value_changes

--- react_v/exercice_1/src/components/Point/index.jsx
+++ react_v/exercice_1/src/components/Point/index.jsx
@@ -3,9 +3,9 @@ import './Point.css';
 export default function Point(props) {
   return (
     <div className='custom-radio'>
-      <input type="radio" id="radio-1" checked={props.page === 1} disabled={props.page !== 1} readOnly />
-      <input type="radio" id="radio-2" checked={props.page === 2} disabled={props.page !== 2} readOnly />
-      <input type="radio" id="radio-3" checked={props.page === 3} disabled={props.page !== 3} readOnly />
+      <input type="radio" id="radio-1" checked={props.page === 0} disabled={props.page !== 0} readOnly />
+      <input type="radio" id="radio-2" checked={props.page === 1} disabled={props.page !== 1} readOnly />
+      <input type="radio" id="radio-3" checked={props.page === 2} disabled={props.page !== 2} readOnly />
     </div>
   );
 }
