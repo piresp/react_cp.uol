53788b990eef7b3b59fd8c5443b81c9d5a4c0670
piresp <gabrielfpp47@gmail.com>
2023-01-05 01:29:23 -0300

    input_focus
    here we use a hook named useImperative to bound the focus key to a activate function, inside the Login.js we have to declare emailInputRef and passwordInputRef as a ref hook and use the focus in a else if statement and obviusly pass it in the component as ref prop

--- react_VI/section_10/src/components/Login/Login.js
+++ react_VI/section_10/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useContext } from 'react';
+import React, { useState, useEffect, useReducer, useContext, useRef, useImperativeHandle } from 'react';
 
 import Card from '../UI/Card/Card';
 import Input from '../UI/Input/Input';
@@ -27,12 +27,7 @@ const passwordReducer = (state, action) => {
 }
 
 const Login = (props) => {
-  // const [enteredEmail, setEnteredEmail] = useState('');
-  // const [emailIsValid, setEmailIsValid] = useState();
-  // const [enteredPassword, setEnteredPassword] = useState('');
-  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
-
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: '', isValid: null
   });
@@ -41,6 +36,9 @@ const Login = (props) => {
   })
 
   const authCtx = useContext(AuthContext);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
@@ -62,18 +60,10 @@ const Login = (props) => {
 
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: 'USER_INPUT', val: event.target.value })
-
-    //setFormIsValid(
-    //  emailState.isValid && passwordState.isValid
-    //);
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({ type: 'USER_INPUT', val: event.target.value });
-
-    //setFormIsValid(
-    //  emailState.isValid && passwordState.isValid
-    //);
   };
 
   const validateEmailHandler = () => {
@@ -86,13 +76,21 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
+
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           id="email"
           label="E-mail"
           type="email"
@@ -102,6 +100,7 @@ const Login = (props) => {
           onBlur={validateEmailHandler}
         />
         <Input
+          ref={passwordInputRef}
           id="password"
           label="Password"
           type="password"
@@ -111,7 +110,7 @@ const Login = (props) => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn} >
             Login
           </Button>
         </div>
