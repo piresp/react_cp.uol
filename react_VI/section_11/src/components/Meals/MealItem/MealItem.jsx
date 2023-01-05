b6233c5fbf35e265120e85337e6cf199a0b2c013
piresp <gabrielfpp47@gmail.com>
2023-01-05 20:13:14 -0300

    passing_items_to_cart

--- react_VI/section_11/src/components/Meals/MealItem/MealItem.jsx
+++ react_VI/section_11/src/components/Meals/MealItem/MealItem.jsx
@@ -1,8 +1,20 @@
+import { useContext } from 'react';
 import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css'
+import CartContext from '../../../store/cart-context';
 
 export default function MealItem(props) {
+  const cartCtx = useContext(CartContext)
   const price = `$${props.price.toFixed(2)}`
+
+  const addToCartHandler = amount => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  }
 
   return (
     <li className={classes.meal}>
@@ -12,7 +24,7 @@ export default function MealItem(props) {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm onAddToCart={addToCartHandler}/>
       </div>
     </li>
   );
