8c7246189d512bd28fb573ca63e1cffbbc99410f
piresp <gabrielfpp47@gmail.com>
2023-01-12 13:47:44 -0300

    applying_redux

--- react_VII/section_19/src/components/Shop/Products.js
+++ react_VII/section_19/src/components/Shop/Products.js
@@ -1,16 +1,42 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 6,
+    title: 'My first book',
+    description: 'The first book I ever wrote'
+  },
+  {
+    id: 'p2',
+    price: 8,
+    title: 'Doritos',
+    description: 'I miss u so much since your just cost 7 reais the medium package'
+  },
+  {
+    id: 'p3',
+    price: 10,
+    title: 'Pickle jar',
+    description: 'A bunch of pickles'
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map(product => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+
       </ul>
     </section>
   );
