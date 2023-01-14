fac47c0cb88dda1238f3bce299c050bb4a796901
piresp <gabrielfpp47@gmail.com>
2023-01-13 21:32:58 -0300

    conditional_route

--- react_VII/section_20/src/pages/QuoteDetail.jsx
+++ react_VII/section_20/src/pages/QuoteDetail.jsx
@@ -1,11 +1,24 @@
 import { useParams, Route } from "react-router";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning react is fun!'},
+  { id: 'q2', author: 'Maximilian', text: 'Learning!'},
+  { id: 'q3', author: 'Gabriel', text: 'Learning react is funtastic!'}
+]
 
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+  if (!quote) {
+    return <p>No quote found!</p>
+  }
+
   return (
     <>
-      <h1>Quotes Details Page {params.quoteId}</h1>
+      <HighlightedQuote text={quote.text} author={quote.author}/>
       <Route path='/quotes/:quoteId/comments'>
         <Comments />
       </Route>
