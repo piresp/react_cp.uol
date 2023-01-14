a4a2aa419d5e72973010d0540a48291ec4af8040
piresp <gabrielfpp47@gmail.com>
2023-01-13 21:41:37 -0300

    dummy_data

--- react_VII/section_20/src/pages/AllQuotes.jsx
+++ react_VII/section_20/src/pages/AllQuotes.jsx
@@ -1,5 +1,13 @@
+import QuoteList from '../components/quotes/QuoteList'
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning react is fun!'},
+  { id: 'q2', author: 'Maximilian', text: 'Learning!'},
+  { id: 'q3', author: 'Gabriel', text: 'Learning react is funtastic!'}
+]
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>
+  return <QuoteList quotes={DUMMY_QUOTES} />
 }
 
 export default AllQuotes
